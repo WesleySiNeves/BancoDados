1af9b49b9129ed73c274de8cc006787f2e094ed7
Wesley Neves <wesley.neves@implantainformatica.com.br>
2021-03-19 14:05:43 -0300

    alterações

---
+++ PowerShell/Automation/ScriptsPowerShell/BancoDeDados/DeletarBancoSqlAzure.ps1
@@ -0,0 +1,50 @@
+
+$databases_a_deletar =
+'cau-sp.implanta.net.br_Copy',
+'cau-sp.implanta.net.br_CopyBackup',
+'cra-es.implanta.net.br_2021-01-10T10-09Z',
+'cra-es.implanta.net.br_2021-02-10T08-00Z',
+'cra-es.implanta.net.br_2021-02-22T08-15Z',
+'cra-pi.implanta.net.br_2021-01-15T14-02Z',
+'cra-rs.implanta.net.br_2020-09-01T02-04Z',
+'cress-mg.implanta.net.br_2021-02-17T08-30Z',
+'cress-rs.implanta.net.br_2021-02-04T08-23Z',
+'crmv-rs.implanta.net.br_Copy',
+'cro-df.implanta.net.br_2021-01-17T13-25Z',
+'cro-mg.implanta.net.br_COPY',
+'cro-pi.implanta.net.br_2021-02-22T08-30Z',
+'cro-pr.implanta.net.br_2021-01-18T20-18Z',
+'cro-rs.implanta.net.br_2021-02-16T08-31Z',
+'crtr-sp.implanta.net.br_COPY',
+'oab-ba.implanta.net.br_2021-02-12T10-30Z',
+'oab-ba.implanta.net.br_2021-02-12T18-04Z',
+'oab-rs.implanta.net.br_2021-02-16T13-03Z';
+
+#Variaveis de definição
+
+$ServerName = 'rgprd-sqlsrv-prd01'
+$resourceGroupName = 'RgPrd'
+
+$User = 'implanta.net'
+$pass = 'M@st3rP0w3r@zur3Prd'
+$securePassword = ConvertTo-SecureString -String $pass -AsPlainText -Force
+$waitDelay = 10
+
+
+  foreach($dbName in $databases_a_deletar)
+  {
+     
+
+       $database = Get-AzSqlDatabase -ResourceGroupName $resourceGroupName  -ServerName $ServerName -DatabaseName $dbName
+
+       if($database)
+       {
+        write-host -Foregroundcolor Yellow $database.DatabaseName
+        Remove-AzSqlDatabase  -ResourceGroupName $resourceGroupName -ServerName $ServerName  -DatabaseName $dbName
+
+       }
+       else {
+        write-host -Foregroundcolor Red $dbName     
+       }
+
+  }
