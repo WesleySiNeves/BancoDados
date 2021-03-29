659d3b02809a39a378752dd44a8b33e1a29837cc
Wesley Neves <wesley.neves@implantainformatica.com.br>
2021-03-29 08:43:12 -0300

    alterações de scripts

--- PowerShell/Automation/ScriptsPowerShell/BancoDeDados/importarBacpac.ps1
+++ PowerShell/Automation/ScriptsPowerShell/BancoDeDados/importarBacpac.ps1
@@ -4,8 +4,11 @@ $Resource ='RgPrd'
 $ServerName ='rgprd-sqlsrv-prd01'
 $DatabaseName ='cau-sp.implanta.net.br'
 
-$StorageKeytype ='StorageAccessKey'
+$database = Get-AzSqlDatabase -ResourceGroupName $Resource  -ServerName $ServerName  -DatabaseName $DatabaseName
 
+
+
+$StorageKeytype ='StorageAccessKey'
 $StorageKey ='6XDERJnF6V34xAKomrppUbbOXhQRwkkb6Yz9KBrQvjvzVf+Ob+Pgi+kV9s0zE9FY8E4Wvwg7QEfDiT3JQmD1aw=='
 $BacpacUri ='https://imdevblob02.blob.core.windows.net/export/'+$DatabaseName +'_Export.bacpac'
 
@@ -14,9 +17,9 @@ $User = 'wesley.neves@implantainformatica.com.br'
 $pass = 'karina_96086512'
 $securePassword = ConvertTo-SecureString -String $pass -AsPlainText -Force
 
-$exportRequest = New-AzSqlDatabaseExport -ResourceGroupName $Resource -ServerName $ServerName -DatabaseName $DatabaseName -StorageKeyType $StorageKeytype -StorageKey $StorageKey -StorageUri $BacpacUri  -AdministratorLogin $User -AdministratorLoginPassword $securePassword
+$exportRequest = New-AzSqlDatabaseExport -ResourceGroupName $Resource -ServerName $ServerName-DatabaseName $DatabaseName -StorageKeyType $StorageKeytype -StorageKey $StorageKey -StorageUri $BacpacUri  -AdministratorLogin $User -AdministratorLoginPassword $securePassword
 
-$waitDelay =5
+
 $exportStatus = Get-AzSqlDatabaseImportExportStatus -OperationStatusLink $exportRequest.OperationStatusLink    
 
     while ($exportStatus.Status -eq "InProgress")
@@ -29,11 +32,11 @@ $exportStatus = Get-AzSqlDatabaseImportExportStatus -OperationStatusLink $export
 
     if($Status -eq "Succeeded")
     {
-        Write-Output "Azure SQL DB Export $Status for "$database""
+        Write-Output "Azure SQL DB Export $Status for $database"
     }
     else
     {
-        Write-Output "Azure SQL DB Export Failed for "$database""
+        Write-Output "Azure SQL DB Export Failed for $database"
     } 
 
     Write-Output "Finalizado"
---
+++ PowerShell/Automation/ScriptsPowerShell/ElasticPool/AlteraBancosEntreOsPools.ps1
@@ -0,0 +1,85 @@
+
+$databases_a_migrar = 'apresentacao-siscaf.implanta.net.br',
+'cro-mt.implanta.net.br',
+'cro-pa.implanta.net.br',
+'cro-pb.implanta.net.br',
+'cro-pe.implanta.net.br',
+'cro-pi.implanta.net.br',
+'cro-es.implanta.net.br',
+'cro-go.implanta.net.br',
+'cro-ma.implanta.net.br',
+'cro-ac.implanta.net.br',
+'cro-al.implanta.net.br',
+'cro-am.implanta.net.br',
+'cro-ap.implanta.net.br',
+'cro-ba.implanta.net.br',
+'cro-ce.implanta.net.br',
+'cro-df.implanta.net.br',
+'cro-rj.implanta.net.br',
+'cro-rn.implanta.net.br',
+'cro-ro.implanta.net.br',
+'cro-rr.implanta.net.br',
+'cro-rs.implanta.net.br',
+'cro-sc.implanta.net.br',
+'cro-se.implanta.net.br',
+'cro-to.implanta.net.br',
+'cra-rj.implanta.net.br',
+'cra-mg.implanta.net.br',
+'cra-ms.implanta.net.br',
+'cra-es.implanta.net.br',
+'cra-ap.implanta.net.br',
+'cra-ac.implanta.net.br',
+'cra-rr.implanta.net.br',
+'cress-to.implanta.net.br',
+'cress-ac.implanta.net.br',
+'cress-se.implanta.net.br',
+'cress-rn.implanta.net.br',
+'cress-pi.implanta.net.br',
+'cress-ms.implanta.net.br',
+'cress-mt.implanta.net.br',
+'cress-pa.implanta.net.br',
+'cress-pb.implanta.net.br',
+'cress-df.implanta.net.br',
+'cress-es.implanta.net.br',
+'cress-go.implanta.net.br',
+'cress-ma.implanta.net.br',
+'cress-ba.implanta.net.br';
+
+#Variaveis de definição
+
+$ServerName = 'rgprd-sqlsrv-prd01'
+$TargetElasticPoolName ='rgprd-elspool-prd01'
+
+
+$resourceGroupName = 'RgPrd'
+
+
+
+  foreach($dbName in $databases_a_migrar)
+  {
+       
+
+       $database = Get-AzSqlDatabase -ResourceGroupName $resourceGroupName  -ServerName $ServerName -DatabaseName $dbName
+
+       if($database)
+       {
+
+          if($database.ElasticPoolName -ne $TargetElasticPoolName)
+          {
+               write-host -Foregroundcolor Yellow $database.DatabaseName, $database.ElasticPoolName;
+               Set-AzSqlDatabase -ResourceGroupName $resourceGroupName `
+               -ServerName $serverName `
+               -DatabaseName $dbName `
+               -ElasticPoolName $TargetElasticPoolName
+
+          }
+          else {
+               write-host -Foregroundcolor Green $dbName   
+          }
+
+       }
+       else {
+        write-host -Foregroundcolor Red $dbName     
+       }
+
+  }
